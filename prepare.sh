cd4ee60187d601af251ce82386525f8e7900ad7a
Thiago Varasquim <varasquim@gmail.com>
2026-05-18 09:57:37 -0300

    Change SSL generate

--- prepare.sh
+++ prepare.sh
@@ -20,6 +20,6 @@ mkdir ./record >/dev/null 2>&1
 chmod -R 777 ./record
 echo "done"
 echo "Creating SSL certificates"
-openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
+openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=BR/ST=Parana/L=Londrina/O=Guacamole/OU=TI/CN=localhost/emailAddress=admin@localhost'
 echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
 echo "done"
